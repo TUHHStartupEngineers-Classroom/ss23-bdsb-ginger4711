1a51986a4e4cbedb913d05a1ee4f03b51f4ba061
ginger4711 <chrischi.suehl@gmail.com>
2023-05-01 15:28:06 +0200

    Data Wrangling
    Reworked first and second exercise. Started working on third.

--- src/DATA_WRANGLING_CHALLENGE.R
+++ src/DATA_WRANGLING_CHALLENGE.R
@@ -10,7 +10,7 @@ library(vroom)
 col_types_patent <- list(
   id = col_character(),
   date = col_date("%Y-%m-%d"),
-  num_claims = col_double()
+  num_claims = col_integer()
 )
 patent_tbl <- vroom(
   file       = "src/Patent_data_reduced/patent.tsv", 
@@ -18,6 +18,7 @@ patent_tbl <- vroom(
   col_types  = col_types_patent,
   na         = c("", "NA", "NULL")
 )
+patent_tbl <- rename(patent_tbl,patent_id = id) # Rename id to patent_id to make merging easier
 
 # loading assignee.tsv file
 col_types_assignee <- list(
@@ -59,10 +60,40 @@ uspc_tbl <- vroom(
 )
 
 ## Patent Dominance ##
+# Convert patent_assignee_tbl to data.table and group it's entries by assignee_id. Then the number of elements per group is stored in a column patents.
+patent_dominance_dt <- as.data.table(patent_assignee_tbl)[,.(patents = .N),by = assignee_id]
+# Combine patent_activity_dt with assignee_tbl by patent_id to combine assignee info with company-name and -type info.
+patent_dominance_dt <- as.data.table(merge(as_tibble(patent_dominance_dt),assignee_tbl,by="assignee_id")) %>%
+                                    .[type == 2] %>% # Then the data table can be filtered by only allowing US Companies or Corporations
+                                    .[order(-patents),patents,organization] # Afterwards the list is sorted by the number of patents.
+                                                                            # Only the patents and organization name column is kept for better looking output.
+print("What US company / corporation has the most patents?")
+print(patent_dominance_dt[1]) # Take the first element of the ordered list.
+print("List the 10 US companies with the most assigned/granted patents.")
+print(patent_dominance_dt[1:10]) # Take the first 10 elements of the ordered list.
 
-# Convert patent_assignee_tbl to data.frame
-patent_dominance_dt <- as.data.table(patent_assignee_tbl)
-# Group by assignee_id and create sum "patents" over those with same assignee_id. Order by patents to take the first 10 elements.
-patent_dominance_dt <- patent_dominance_dt[,.(patents = .N),by = assignee_id][order(-patents),head(.SD, 10)]
-# Merge with assignee_tbl by assignee_id, then order by descending patents again and only keep patents and organization column and then print.
-patent_dominance_dt <- as.data.table(merge(as.tibble(patent_dominance_dt),assignee_tbl,by="assignee_id"))[order(-patents),patents,organization] %>% print()
+## Recent Patent Activity ##
+# Take patent_tbl and separate into year, month and day and convert to data.table.
+patent_activity_dt <- patent_tbl %>% separate(col = date,into = c("year","month","day"),sep = "-",convert = T) %>%
+                                 as.data.table() %>% .[year == 2014 & month == 8] # Then filter by august (8) 2014.
+# Combine patent_activity_dt with patent_assignee_tbl by patent_id to combine patent info with assignee info..
+patent_activity_dt <- as.data.table(merge(as_tibble(patent_activity_dt),patent_assignee_tbl,by="patent_id")) %>%
+                                   .[,.(patents = .N),by = assignee_id] # Group and sum up by assignee_id.
+# Combine patent_activity_dt with assignee_tbl by patent_id to combine assignee info with company-name and -type info.
+patent_activity_dt <- as.data.table(merge(as_tibble(patent_activity_dt),assignee_tbl,by="assignee_id")) %>%
+                                    .[order(-patents),.(organization,type,patents)] # Order by number of patents and discard assignee_id.
+
+print("What US company had the most patents granted in August 2014?")
+print(patent_activity_dt[type == 2][1]) # Filter by US Company or Corporation and take the first element of the ordered list.
+print("List the top 10 companies with the most new granted patents for August 2014.")
+print(patent_activity_dt[1:10]) # Take the first 10 elements of the ordered list.
+
+
+## Innovation In Tech ##
+innovative_dt <- as.data.table(merge(patent_assignee_tbl,uspc_tbl,by="patent_id")) %>%
+                              .[,.(patents = .N),by = mainclass_id]
+
+
+
+print("What is the most innovative tech sector?")
+print("For the top 10 companies (worldwide) with the most patents, what are the top 5 USPTO tech main classes?")
